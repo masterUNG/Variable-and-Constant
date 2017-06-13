40fd39b22597188045fae9d60779d30ee8b31fe1
teacher <phrombutr@gmail.com>
2017-06-12 17:09:40 -0700

    VarOK

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -20,6 +20,66 @@ print(intNumber)    // Display only Value
 print("intNumber ==> \(intNumber)")
 
 
+//Explicit String  การประกาศแบบ กำหนด
+//Data Type ==> String
+let strName: String = "Doramon"
+
+//Implicit String Datatype ==> Stiong
+let strSurname = "Japane"
+
+//Operate
+var strOfficer = strName + " " + strSurname
+strOfficer = "Mr." + strOfficer
+
+//Operating on Number
+let intA = 5
+let intB = 2
+
+var answer = intA + intB
+answer = intA * intB
+answer = intA / intB
+
+//Change Datatype Integer to Double
+var douA = Double(intA)
+var douB = Double(intB)
+
+var answer2: Double = douA / douB
+
+var myStatus = true //Implicit
+var myStatus2: Bool = false //Explicit
+
+//print on Console ==> ถ้าเอา 5 บวก 6 เท่ากับ 11
+
+//String Manage
+let strTitle1 = "ถ้าเอา"
+let strTitle2 = "เท่ากับ"
+let strAdd = "บวก"
+let strHand = "หาร"
+let strSpace = " "
+
+//Int Manage
+let intNum1 = 5
+let intNum2 = 6
+
+print(strTitle1 + strSpace + "\(intNum1)" + strSpace + strAdd + strSpace + "\(intNum2)" + strSpace + strTitle2 + strSpace + "\(intNum1 + intNum2)")
+
+print(strTitle1 + strSpace + "\(intNum1)" + strSpace + strHand + strSpace + "\(intNum2)" + strSpace + strTitle2 + strSpace + "\(Double(intNum1) / Double(intNum2))")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
